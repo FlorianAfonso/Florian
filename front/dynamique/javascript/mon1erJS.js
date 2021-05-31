6d278433d682422797b24aee6a76acd1ed89b4e7
Afonso Florian <florian.afonso.afpa@gmail.com>
2021-05-31 10:23:32 +0200

    debut Javascript

---
+++ front/dynamique/javascript/mon1erJS.js
@@ -0,0 +1,10 @@
+var MyVar = "Bonjour";
+window.alert(MyVar);
+var reponse1 = window.prompt("Saisissez votre nom");
+var reponse2 = window.prompt("Saisissez votre prénom");
+if (window.confirm("Etes-vous un homme ?") == true) 
+{ 
+
+}
+var MyVar = "Bienvenue sur notre site web !"
+window.alert(MyVar);
