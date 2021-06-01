aa49460da8dff2d418601c8933fc0f55f0f1f322
Afonso Florian <florian.afonso.afpa@gmail.com>
2021-06-01 14:37:19 +0200

    ajout js06

--- front/dynamique/javascript/JS06.js
+++ front/dynamique/javascript/JS06.js
@@ -1,3 +1,21 @@
-var a = "100"; b = 100; c = 1,00; d = true
-document.write("Ceci est une chaîne de caractères :"+a);
-var b = a--;
+var a ="100";
+var b = 100;
+var c = 1.00;
+var d = true;
+
+
+document.write("Ceci est une chaîne de caractères :" + a +'</br>') ;
+document.write("Valeur de b avant la décrémentation :" + b +'</br>') ;
+b--;
+document.write("Valeur de b aprés la décrémentation :" + b +'</br>') ;
+
+
+var e = a + c;
+
+document.write("Ajout de la valeur de c à a :" + e +'</br>') ;
+
+document.write("valeur de d  :" + d +'</br>') ;
+
+ 
+
+document.write("valeur de d aprés inversion avec !d :" + d +'</br>') ;
