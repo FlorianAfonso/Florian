cd916b2f5811ab99105a5163a508432fd4458b60
Afonso Florian <florian.afonso.afpa@gmail.com>
2021-06-14 13:08:50 +0200

    exercice evaluation 5 . terminé

--- front/dynamique/javascript/evaluation/Exercice_5.js
+++ front/dynamique/javascript/evaluation/Exercice_5.js
@@ -3,8 +3,6 @@ var champ_nom = form_inscription.elements["nom"];
 var champ_prenom = form_inscription.elements["prenom"];
 var champ_email = form_inscription.elements["mail"];
 var champ_cp = form_inscription.elements["cp"];
-//var regExMail = /^[a-zA-Z]+\.?(\w+)?\d+@[a-zA-Z]+\.[a-z]+/
-
 var form_OK = true;
 
 form_inscription.addEventListener('submit', (e)=>{
@@ -18,21 +16,19 @@ if(champ_cp.value.length != 5 || isNaN(champ_cp.value)){
 }
 
 if(champ_nom = ""){
-    form_OK = false;
     alert("Veuillez renseigner votre Nom s.v.p.");
 }else if(!form_OK){
     e.preventDefault();
 }
 
 if(champ_prenom = ""){
-    form_OK = false;
     alert("Veuillez renseigner votre Prénom s.v.p.");
 }else if(!form_OK){
     e.preventDefault();
 }
 
 if( !champ_email.value.includes("@")){
-    alert('c\'est pas la bonne ');
+    alert("Veuillez renseigner une adresse E-mail valide s.v.p.");
     e.preventDefault();
 }
 
