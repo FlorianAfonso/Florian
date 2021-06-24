7a6085e42ad3ded71b718f11697a3ca289538820
Afonso Florian <florian.afonso.afpa@gmail.com>
2021-06-24 15:26:56 +0200

    ajout php

---
+++ back/serveur/php/php_les_tableaux/exercice1.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+    <?php
+    $a = array("19001" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "", "", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", "Validation"), 
+    "19002" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", ""), 
+    "19003" => array("", "", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "", "", "Validation") 
+   );
+
+   $arr = $a["19002"];
+   $i = 0;
+   foreach($arr as $sub_arr){
+       $i++;
+       if( $sub_arr == "Validation"){
+       echo $i;
+       break;
+   }
+   }
+    ?>
+</body>
+</html>
