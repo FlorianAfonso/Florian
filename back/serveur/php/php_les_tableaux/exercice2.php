80c7894061aa7a457ebf6b197471f19f2010d522
Afonso Florian <florian.afonso.afpa@gmail.com>
2021-06-24 16:11:18 +0200

    php update

--- back/serveur/php/php_les_tableaux/exercice2.php
+++ back/serveur/php/php_les_tableaux/exercice2.php
@@ -6,15 +6,20 @@
     "19003" => array("", "", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "", "", "Validation") 
    );
 
-   $arr = $a["19002"];
-   $i = 0;
-   foreach($arr as $sub_arr){
-       $i++;
-       if( $sub_arr == "Validation"){
-       echo "La semaine de validation pour le 2ème groupe est la $i<sup>ème</sup> semaine.";
-       break;
-   }
-   }
-    ?>
+   $der = $a["19001"] ;
+   $i = 0 ;
+   foreach ($der as $sub_der)
+{
+    $i++ ;
+    if ($sub_der == "Validation")
+    {
+        $i-- ;
+        echo $i ;
+        break ;
+    }
+}
+   
+   ?>
 </body>
 </html>
+   
--- back/serveur/php/php_les_tableaux/exercice4.php
+++ back/serveur/php/php_les_tableaux/exercice4.php
@@ -6,15 +6,18 @@
     "19003" => array("", "", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "", "", "Validation") 
    );
 
-   $arr = $a["19002"];
-   $i = 0;
-   foreach($arr as $sub_arr){
-       $i++;
-       if( $sub_arr == "Validation"){
-       echo "La semaine de validation pour le 2ème groupe est la $i<sup>ème</sup> semaine.";
-       break;
-   }
-   }
-    ?>
+$stage = $a["19003"];
+$i = 0 ;
+        foreach($stage as $sub_stage)
+        {
+            if($sub_stage == "Stage")
+            {
+                $i++ ;
+            }
+        }
+echo $i ;
+
+?>
 </body>
 </html>
+   
