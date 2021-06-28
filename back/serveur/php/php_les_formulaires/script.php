89b7aecdf3cccc4f32d2cabd0e59323e7bdfd6c1
Afonso Florian <florian.afonso.afpa@gmail.com>
2021-06-28 16:15:15 +0200

    update php les formulaires

--- back/serveur/php/php_les_formulaires/script.php
+++ back/serveur/php/php_les_formulaires/script.php
@@ -2,32 +2,41 @@
 <body>
   <?php 
 
-    $nom = $_GET["nom"];
-    $prenom = $_GET["prenom"];  
-    $birthdate = $_GET["start"];
-    $postcode = $_GET["cp"];
-    $email = $_GET["mail"];
+    // $nom = "nom";
+    // $prenom = "prenom";  
+    // $birthdate = "start";
+    // $postcode = "cp";
+    // $email = "mail";
+ 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      // collect value of input field
+      $name = $_POST['user_name'];
+      if (empty($name)) {
+        echo "Name is empty";
+      } else {
+        echo $name;
+      }
+    }
+    // $prenom = $_POST['prenom'];  
+    // $nom = $_POST['nom'];
+    // echo $nom;   
+  //    if (empty($_POST["prenom"]))
+  //   {
+  //       echo"Le prénom doit être renseigné <br>";
+  // }
 
-    if (empty($_POST["nom"]))
-    {
-        echo"Le nom doit être renseigné";
-    }
-  
-    if (empty($_POST["prenom"]))
-    {
-        echo"Le prénom doit être renseigné";
-    }
+  //    if (empty($_POST["start"]))
+  //   {
+  //      echo"L'email doit être renseigné <br>";
+  // }
 
-    if (empty($_POST["start"]))
-    {
-        echo"l'Email doit être renseigné";
-    }
-  
-    if (empty($_POST["cp"]))
-    {
-        echo"Le code postale doit être renseigné";
-    }
-  
+  //    if (empty($_POST["cp"]))
+  //   {
+  //      echo"Le code postale doit être renseigné <br>";
+  // }
+
+
   ?> 
 </body>
 </html>
+
