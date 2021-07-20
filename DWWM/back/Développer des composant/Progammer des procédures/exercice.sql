97ab36615dcf17157f5a6cc061512150c035568f
FlorianAfonso <florian.afonso.afpa@gmail.com>
2021-07-20 16:23:42 +0200

    ajout exercice et fin exo procedure

--- DWWM/back/Développer des composant/Progammer des procédures/exercice.sql
+++ DWWM/back/Développer des composant/Progammer des procédures/exercice.sql
@@ -38,4 +38,19 @@ DELIMITER ;
 /*Exercice 3 : création d'une procédure stockée avec plusieurs paramètres*/
 
 
+DELIMITER |
 
+CREATE PROCEDURE CA_Supplier(
+    p_sup_id INT(10),
+    p_year CHAR(4)
+)
+
+BEGIN
+    SELECT SUM(ode_unit_price * ode_quantity) FROM gescom_afpa.orders_details
+    JOIN gescom_afpa.products ON ode_pro_id = pro_id
+    JOIN gescom_afpa.suppliers ON pro_sup_id = sup_id
+    JOIN gescom_afpa.orders ON ode_ord_id = ord_id
+    WHERE sup_id = p_sup_id AND ord_payment_date LIKE CONCAT(p_year, '-%-%');
+END |
+
+DELIMITER ;
