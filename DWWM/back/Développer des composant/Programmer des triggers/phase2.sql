c26ef0a936bea56b3c8b6d4c263dcd1c3880a601
FlorianAfonso <florian.afonso.afpa@gmail.com>
2021-07-21 15:26:05 +0200

    correction exercice 3 phase 2 les triggers

--- DWWM/back/Développer des composant/Programmer des triggers/phase2.sql
+++ DWWM/back/Développer des composant/Programmer des triggers/phase2.sql
@@ -25,16 +25,27 @@ BEGIN
     DECLARE v_pro_id INT(10);
     DECLARE v_pro_stock INT(5);
     DECLARE v_pro_stock_alert INT(5);
+    DECLARE v_codart INT(10);
 
     SET v_pro_id = NEW.pro_id;
     SET v_pro_stock = NEW.pro_stock;
     SET v_pro_stock_alert = NEW.pro_stock_alert;
+    SET v_codart = (SELECT codart FROM commander_articles WHERE codart = v_pro_id);
 
     IF (v_pro_stock < v_pro_stock_alert)
     THEN
-        INSERT INTO commander_articles 
-        VALUES
-        (v_pro_id, v_pro_stock_alert - v_pro_stock, now());
+        IF ISNULL(v_codart)
+        THEN
+            INSERT INTO commander_articles 
+            VALUES
+            (v_pro_id, v_pro_stock_alert - v_pro_stock, now());
+        ELSE
+            UPDATE commander_articles
+            SET
+                qte = v_pro_stock_alert - v_pro_stock,
+                date = now()
+            WHERE codart = v_codart;
+        END IF;
     END IF;
 END |
 
