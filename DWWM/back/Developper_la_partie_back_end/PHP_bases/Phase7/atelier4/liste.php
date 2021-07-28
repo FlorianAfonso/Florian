b8ce317a57428a31987881a0e88374db7307058a
FlorianAfonso <florian.afonso.afpa@gmail.com>
2021-07-28 16:13:12 +0200

    ajout exo php avancé phase 8

--- DWWM/back/Developper_la_partie_back_end/PHP_bases/Phase7/atelier4/liste.php
+++ DWWM/back/Developper_la_partie_back_end/PHP_bases/Phase7/atelier4/liste.php
@@ -1,43 +1,65 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-<meta charset="UTF-8">
-<title>Atelier PHP N°4 - Affichage de la liste</title>
-</head>
-<body> 
-<div class="container">
 <?php
-require "connexion_bdd.php"; // Inclusion de notre bibliothèque de fonctions
-$db = connexionBase(); // Appel de la fonction de connexion
-$requete = "SELECT pro_id, pro_ref, pro_libelle FROM produits WHERE ISNULL(pro_bloque) ORDER BY pro_d_ajout DESC";
+$DATABASE_HOST = 'localhost';
+$DATABASE_NAME = 'jarditou';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
 
-$result = $db->query($requete);
-
-if (!$result) 
+try
 {
-    $tableauErreurs = $db->errorInfo();
-    echo $tableauErreur[2]; 
-    die("Erreur dans la requête");
-}
-
-if ($result->rowCount() == 0) 
+    $db = new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $answer = $db->query('SELECT * FROM produits');
+} 
+catch (PDOException $exception) 
 {
-   // Pas d'enregistrement
-   die("La table est vide");
+    echo $exception->getMessage() . "<br>";
+    die();
 }
-
-echo "<table>";
-
-while ($row = $result->fetch(PDO::FETCH_OBJ))
-{
-    echo"<tr>";
-    echo"<td>".$row->pro_id."</td>";
-    echo"<td>".$row->pro_ref."</td>";
-    echo"<td><a href=\"detail.php?id=".$row->pro_id."\" title=\"".$row->pro_libelle."\"></a></td>";
-    echo"</tr>";
-}
-
-echo "</table>"; 
 ?>
-</body>
-</html> 
+
+<section>
+    <div class="table-responsive table-striped table-hover table-bordered">
+        <table class="table text-center">
+            <thead class="h4 thead-light">
+            <tr>
+                <th scope="col">Photo</th>
+                <th scope="col">ID</th>
+                <th scope="col">Référence</th>
+                <th scope="col">Libellé</th>
+                <th scope="col">Prix</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Couleur</th>
+                <th scope="col">Date d'ajout</th>
+                <th scope="col">Date de modification</th>
+                <th scope="col">Statut</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php while ($row = $answer->fetch(PDO::FETCH_OBJ)): ?>
+                <tr>
+                    <td><img src="src/images/<?php echo "$row->pro_id.$row->pro_photo"; ?>" class="img-fluid" alt="<?php echo $row->pro_libelle; ?>" title="<?php echo $row->pro_libelle; ?>"></td>
+                    <td><?php echo $row->pro_id; ?></td>
+                    <td><?php echo $row->pro_ref; ?></td>
+                    <td><u><a class="font-weight-bold text-danger" href="details.php?pro_id=<?php echo $row->pro_id ?>"><?php echo $row->pro_libelle ?></a></u></td>
+                    <td><?php echo $row->pro_prix; ?>€</td>
+                    <td><?php echo $row->pro_stock; ?></td>
+                    <td><?php echo $row->pro_couleur; ?></td>
+                    <td><?php echo $row->pro_d_ajout; ?></td>
+                    <td><?php echo $row->pro_d_modif; ?></td>
+                    <td>
+                        <?php 
+                        if ($row->pro_bloque == 1)
+                        {
+                            echo 'Oui';
+                        }
+                        ?>
+                    </td>
+                    <?php 
+                    endwhile; 
+                    $answer->closeCursor();
+                    ?>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</section>
