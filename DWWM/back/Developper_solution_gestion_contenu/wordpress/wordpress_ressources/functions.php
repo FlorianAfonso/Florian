ecee280b3a059e6e26b1fdae379445cb6e927fdc
FlorianAfonso <florian.afonso.afpa@gmail.com>
2021-09-29 13:37:30 +0200

    debut du cour sur Wordpress , besoin pour le stage

---
+++ DWWM/back/Developper_solution_gestion_contenu/wordpress/wordpress_ressources/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function youpi_custom_logo_setup() 
+{
+   $aParams = array(
+      'height'      => 100,
+      'width'       => 400,
+      'flex-height' => true,
+      'flex-width'  => true,
+      'header-text' => array( 'site-title', 'site-description' ),
+   );
+
+   // Ajout du support 
+   add_theme_support('custom-logo', $aParams );
+}
+
+add_action( 'after_setup_theme', 'youpi_custom_logo_setup' );
+
+?>
+
+add_theme_support('menus');  
